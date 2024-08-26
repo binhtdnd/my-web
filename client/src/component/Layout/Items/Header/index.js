3edd7ed8f13743434258ac817c83334ef11db5fc
binhtdnd <binhtdnd@gmail.com>
2024-08-26 15:08:05 +0700

    fix to build, delele fix cors

--- client/src/component/Layout/Items/Header/index.js
+++ client/src/component/Layout/Items/Header/index.js
@@ -8,7 +8,7 @@ class Header extends Component {
             <div>
 
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <Link className="navbar-brand" to="/">Alice 3.21</Link>
+                    <Link className="navbar-brand" to="/">Alice 1.12</Link>
 
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false">
                         <span className="navbar-toggler-icon"></span>
